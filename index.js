e82b71a3f064c732d4cde984a320fc9dcfbddc05
escowin <edwin@escowinart.com>
2022-01-16 21:37:14 -0600

    readme questions

--- index.js
+++ index.js
@@ -1,52 +1,24 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const generatePage = require('./src/page-template');
-const { writeFile, copyFile } = require('./utils/generate-site');
+const generateReadMe = (projectName, githubName) => {
+    return `
+        Project: ${projectName}
+        GitHub: ${githubName}
+    `;
+};
+console.log(generateReadMe('esco', 'escowin'));
+
 
 
 // TODO: Create an array of questions for user input
-const questions = () => {
-    return inquirer.prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name? (Required)',
-        validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log('Please enter your name!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub Username (Required)',
-        validate: githubInput => {
-          if (githubInput) {
-            return true;
-          } else {
-            console.log('Please enter your GitHub username!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'confirm',
-        name: 'confirmAbout',
-        message: 'Would you like to enter some information about yourself for an "About" section?',
-        default: true
-      },
-      {
-        type: 'input',
-        name: 'about',
-        message: 'Provide some information about yourself:',
-        when: ({ confirmAbout }) => confirmAbout
-      }
-    ]);
-  };
+const questions = [
+    'project title:',
+    'project description:',
+    'installation instructions:',
+    'information usage:',
+    'test instructions:',
+    'github username:'
+]
+console.log(questions);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
